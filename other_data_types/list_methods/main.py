2a6975df72148f9da1750f672a94375874d50a20
osiasyves57 <>
2026-01-18 00:36:28 +0000

    List Methods

---
+++ other_data_types/list_methods/main.py
@@ -0,0 +1,14 @@
+# Initial grocery store inventory
+inventory = ["milk", "bread", "eggs", "cheese"]
+inventory.append("apples")
+inventory.remove("bread")
+inventory.sort()
+# TODO: Add "apples" to the inventory using a list method
+
+# TODO: Remove "bread" from the inventory using a list method
+
+# TODO: Sort the inventory in alphabetical order using a list method
+
+
+# Output the final list
+print(inventory)
