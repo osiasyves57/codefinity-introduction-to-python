90374927e6cb61b460dd2cb70de50fc3d4cbb617
osiasyves57 <>
2026-01-18 00:50:39 +0000

    Challenge: List Management

---
+++ other_data_types/challenge_list_management/main.py
@@ -0,0 +1,8 @@
+meat = ["Ham", 3.99, 50, "Sliced"]
+cheese = ["Cheddar", 5.49, 100, "Sharp"]
+deli_dept = [meat, cheese]
+print("Initial Deli List:", deli_dept)
+if meat[2] < 100:
+    meat[2] = 100
+deli_dept.sort()
+print("Updated Deli list: ", deli_dept)
